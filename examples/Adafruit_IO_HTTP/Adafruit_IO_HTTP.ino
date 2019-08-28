04cc206e28a264700c57ac4730f3328aabaf3fd9
Pan Ziyue <random.rrr3@gmail.com>
2019-08-28 18:11:01 +0800

    fixed Adafruit example

--- examples/Adafruit_IO_HTTP/Adafruit_IO_HTTP.ino
+++ examples/Adafruit_IO_HTTP/Adafruit_IO_HTTP.ino
@@ -16,14 +16,11 @@ void setup()
   Serial.begin(9600);
 
   // Open the link between the two devices
-  Serial.println(F("Opening link to Wi-fi chip..."));
   wifi.openLink();
 
   // Verify that the link is ok between the two devices
-  Serial.print(F("Verifying link ok..."));
-  if (wifi.smokeTest()) Serial.println("ok");
-  else {
-    Serial.println(F("not ok. Halting.")); 
+  if (!wifi.smokeTest()) {
+    Serial.println(F("Unable to establish link with Wi-Fi chip. Halting.")); 
     while (true){};
   }
 
@@ -35,7 +32,9 @@ void setup()
 
 void loop()
 {
-  randNumber = random(0, 255);
+  randNumber = (int)random(0, 255);
+  Serial.print("Random number picked: ");
+  Serial.println(randNumber);
   transmitData(randNumber);
   delay(5000); // 5 second interval
 }
@@ -44,39 +43,20 @@ void connectToWifi(void)
 {
   // Connects to Wifi and displays connection state
   wifi.connectToWifi(SSID, PASSWORD);
-  Serial.print(F("Connecting to wifi with SSID: "));
-  Serial.print(SSID);
-  Serial.print(F(", password: "));
-  Serial.print(PASSWORD);
-  Serial.println(F("..."));
+  Serial.println(F("Connecting to Wi-Fi..."));
 
   delay(10000); // 10 seconds optimal for wifi connection to fully establish
 
   Status wifiStatus = wifi.getWifiStatus();
-  Serial.print(F("Wifi status: "));
-  switch (wifiStatus)
-  {
-  case SUCCESSFUL:
-    Serial.println(F("connected"));
-    break;
-  case UNSUCCESSFUL:
-    Serial.println(F("failed to connect"));
+  if (wifiStatus != SUCCESSFUL) {
+    Serial.println(F("Failed to connect to Wi-Fi"));
     while (true){};
-    break;
-  case IN_PROGRESS:
-    Serial.println(F("connecting"));
-    while (true){};
-    break;
-  case NOT_ATTEMPTED:
-    Serial.println(F("not connected"));
-    while (true){};
-    break;
   }
 }
 
 void transmitData(int value)
 {
-  Serial.println(F("Transmitting data to Adafruit IO via HTTP..."));
+  // Setup the connection
   int handle = wifi.setupHTTP(POST, F("https://io.adafruit.com/api/v2/" IO_USERNAME "/feeds/" FEED_KEY "/data"));
   if (handle == -1) {
     Serial.println(F("Unable to setup HTTP connection!"));
@@ -84,7 +64,9 @@ void transmitData(int value)
   }
 
   // Set POST data
-  if (!wifi.setHTTPPOSTParameters(handle, "value=" + value)) {
+  String combinedString = "value=";
+  combinedString += value;
+  if (!wifi.setHTTPPOSTParameters(handle, combinedString)) {
     Serial.println(F("HTTP unable to set POST parameters!"));
     while (true){};
   }
@@ -96,23 +78,24 @@ void transmitData(int value)
   }
 
   // Actually transmit the data
+  Serial.println(F("Transmitting data to Adafruit IO via HTTP..."));
   if (!wifi.transmitHTTP(handle)) {
       Serial.println(F("Transmit failed!"));
       while (true){};
   }
 
-  // Wait for a good reply
   delay(10000);
+
+  // Check if the message was received properly
   Status http = wifi.getHTTPProgress(handle);
   if (http != SUCCESSFUL) {
     Serial.println(F("HTTP request did not succeed!"));
-  } else {
-    // Print out reply
-    Serial.print(F("Got HTTP status code: "));
-    Serial.println(wifi.getHTTPStatusCode(handle));
-    Serial.print(F("Got HTTP reply: "));
-    Serial.println(wifi.getHTTPReply(handle, CONTENT, false));
   }
+  // Print out the reply in full
+  Serial.print(F("Got HTTP status code: "));
+  Serial.println(wifi.getHTTPStatusCode(handle));
+  Serial.print(F("Got HTTP reply: "));
+  Serial.println(wifi.getHTTPReply(handle, CONTENT, false));
   // Purge the previous HTTP data
   wifi.deleteHTTPReply(handle);
 }
